2ec360e6dd1460bb4da0a7169328d5d0ba3f5034
Marcos G. Zimmermann <mgzmaster@gmail.com>
2012-07-26 22:12:48 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NearbyFriends::Application.config.secret_token = 'fea2ea7e7783d10f48379a2e2ee317ce656cccef1972324d22a38968763577f949fd13dc1732d54ceaf43e4b59e0d1903b0223bc74e576e12c4e373a98554293'
