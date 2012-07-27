5f1c61404cdf7a74733ae517ba808a61f287e49c
Marcos G. Zimmermann <mgzmaster@gmail.com>
2012-07-26 23:19:05 -0300

    Created scope nearby and friends_near_me instance method

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,15 @@
 class Friend < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :lat, :numericality => true
+  validates :lon, :numericality => true
+
+  scope :nearby, lambda { |lat, lon|
+    select(" #{self.table_name}.*, SQRT( POW((69.1 * (#{lat} - #{self.table_name}.lat)), 2) + POW((69.1 * (#{lon} - #{self.table_name}.lon) * cos(#{lat}/57.3)), 2)) AS distance").
+    where("(#{self.table_name}.lat <> 0) AND (#{self.table_name}.lon <> 0) AND (#{self.table_name}.lat IS NOT NULL) AND (#{self.table_name}.lon IS NOT NULL)").
+    order("distance ASC")
+  }
+
+  def friends_near_me
+    self.class.where("id <> ?", self).nearby(self.lat, self.lon).limit(3)
+  end
 end
--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe Friend do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#friends_near" do
+    before(:each) do
+      portugal = {lat: 39.3998, lon: -8.2244}
+      italy    = {lat: 41.8719, lon: 12.5673}
+      ukraine  = {lat: 49.2412, lon: 31.5517}
+      finland  = {lat: 61.9241, lon: 25.7481}
+      mexico   = {lat: 23.6345, lon: -102.5527}
+      canada   = {lat: 56.1303, lon: -106.3467}
+      usa      = {lat: 37.0902, lon: -95.7128}
+      cuba     = {lat: 21.5217, lon: -77.7811}
+      @italy    = FactoryGirl.create(:friend, italy.merge(name: "Italy"))
+      @ukraine  = FactoryGirl.create(:friend, ukraine.merge(name: "ukraine"))
+      @portugal = FactoryGirl.create(:friend, portugal.merge(name: "portugal"))
+      @finland  = FactoryGirl.create(:friend, finland.merge(name: "finland"))
+      @mexico   = FactoryGirl.create(:friend, mexico.merge(name: "mexico"))
+      @canada   = FactoryGirl.create(:friend, canada.merge(name: "canada"))
+      @usa      = FactoryGirl.create(:friend, usa.merge(name: "usa"))
+      @cuba     = FactoryGirl.create(:friend, cuba.merge(name: "cuba"))
+    end
+
+    it "should respont to distance" do
+      @finland.friends_near_me.first.should respond_to(:distance)
+    end
+
+    it "should return 3 friends near finland ordered by distance" do
+      @finland.friends_near_me.map(&:name).should eq(["ukraine", "Italy", "portugal"])
+    end
+
+    it "should return 3 friends near portugal ordered by distance" do
+      @portugal.friends_near_me.map(&:name).should eq(["Italy", "ukraine", "finland"])
+    end
+
+    it "should return 3 friends near mexico ordered by distance" do
+      @mexico.friends_near_me.map(&:name).should eq(["usa", "cuba", "canada"])
+    end
+  end
 end
